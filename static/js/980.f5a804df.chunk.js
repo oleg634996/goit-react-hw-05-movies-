3977d3be2e331c59ce3dd13bf516f39a87911a52
oleg634996 <84518210+oleg634996@users.noreply.github.com>
2022-08-01 19:25:45 +0000

    Deploying to gh-pages from @ oleg634996/goit-react-hw-05-movies-@b43ed3e6eae7fb9d7bf1b3a348a16d6739c09fdb 🚀

---
+++ static/js/980.f5a804df.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[980],{1980:function(e,s,a){a.r(s);var n=a(501),r=a(6871),c=a(184);s.default=function(){return(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)("header",{className:"header",children:(0,c.jsxs)("nav",{className:"nav",children:[(0,c.jsx)(n.OL,{to:"/",className:"link ",children:"Home"}),(0,c.jsx)(n.OL,{to:"movies",className:"link",children:"Movies"})]})}),(0,c.jsx)(r.j3,{})]})}}}]);
+//# sourceMappingURL=980.f5a804df.chunk.js.map
